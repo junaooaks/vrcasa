ddafd46b3ab228e01bac0bac228f82934af22bc7
junaooaks <junaooaks@gmail.com>
2019-10-08 21:17:26 -0400

    teste 2
    teste 2

--- resources/js/components/Home.jsx
+++ resources/js/components/Home.jsx
@@ -177,7 +177,7 @@ export default class Home extends Component {
               </div>
               </div>
               
-             
+                
               
               
               
